868c9bec81298b0dad95dd8aac9e196e712d224c
yangyao <yangy_coder@163.com>
2018-04-09 23:13:27 +0800

    front page build

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,48 @@
 # coding:utf8
 from . import home
+from flask import render_template, redirect, url_for
 
 
 @home.route("/")
 def index():
-    return "<h1>this is home</h1>"
+    return render_template("home/index.html")
+
+
+@home.route("/login")
+def login():
+    return render_template("home/login.html")
+
+
+@home.route("/logout")
+def logout():
+    return redirect(url_for("home.login"))
+
+
+@home.route("/register")
+def register():
+    return render_template('home/register.html')
+
+
+@home.route("/user")
+def user():
+    return render_template("home/user.html")
+
+
+@home.route("/pwd")
+def pwd():
+    return render_template('home/pwd.html')
+
+
+@home.route("/comments")
+def comments():
+    return render_template("home/comments.html")
+
+
+@home.route("/login_log")
+def login_log():
+    return render_template('home/login_log.html')
+
+
+@home.route("/movie_col")
+def movie_col():
+    return render_template('home/movie_col.html')
